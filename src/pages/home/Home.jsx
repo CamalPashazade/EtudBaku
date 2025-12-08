4bcc8e36bda84c9c92087e58c1ce3abb88eea26e
CamalPashazade <camal24011992@gmail.com>
2025-12-08 20:45:12 +0400

    new home video changed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -13,6 +13,8 @@ const about = [
 
 const footer_image = '/home/drinks.avif';
 
+const videoo = '/home/video/web...mp4';
+
 
 
 const Home = () => {
@@ -26,7 +28,8 @@ const Home = () => {
         muted
       >
         <source
-          src="https://player.vimeo.com/progressive_redirect/playback/1128273437/rendition/1080p/file.mp4?loc=external&signature=011db33322e2ef7f9996308d7389bb541671ea4be92ec4b685f112d9bfeb36a4"
+          // src="https://player.vimeo.com/progressive_redirect/playback/1128273437/rendition/1080p/file.mp4?loc=external&signature=011db33322e2ef7f9996308d7389bb541671ea4be92ec4b685f112d9bfeb36a4"
+          src={videoo}
           type="video/mp4"
         />
         <p>
