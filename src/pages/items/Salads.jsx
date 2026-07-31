1071bab878088b42b7a75f3ae01204de7abd0ad0
CamalPashazade <camal24011992@gmail.com>
2026-07-31 22:42:19 +0400

    fixed salads page

--- src/pages/items/Salads.jsx
+++ src/pages/items/Salads.jsx
@@ -1,8 +1,19 @@
 // import NavbarDemo from "@/components/NavbarDemo";
 // import Footer from "@/components/Footer";
+import { useEffect } from "react";
 import './pagesStyles/menuItem.css'
 
 export default function Salads() {
+
+
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "instant" // və ya "smooth"
+    });
+  }, []);
+
   return (
     <>
       {/* <NavbarDemo /> */}
@@ -10,85 +21,88 @@ export default function Salads() {
       <div className="menu-item-page">
 
         <h1>Salads</h1>
+          <div className="page-item-line"></div>
 
         <div className="etud-line"></div>
+<div className="page-item">
+    <div className="item-title">
+        <h3>Smoked Salmon Salad / Hisə verilmiş Qızıl balıq salatı</h3>
+        <p>16.80</p>
+    </div>
 
-        <div className="page-item">
-            <div className="item-title">
-                <h3>Smoked Salmon salad / Hisə verilmiş Qızıl balıq salatı</h3>
-                <p>15.80</p>
-            </div>
+  
 
-            <div className="page-item-line"></div>
-            
-            <p>Smoken salmon, mix salad, mix cheese, lemon sauce, cherry tomato, orange</p>
-            <span><p>Hisə verilmiş qızıl balıq, göyərtilər, limon sousu, çerri pomidoru, portağal</p></span>
-        </div>
+    <p>Smoked salmon, mix salad, mix cheese, lemon sauce, cherry tomato, orange</p>
+    <span><p>Hisə verilmiş qızıl balıq, göyərtilər, limon sousu, çerri pomidoru, portağal</p></span>
+</div>
 
+<div className="page-item">
+    <div className="item-title">
+        <h3>Beef Salad / Can əti salatı</h3>
+        <p>17.80</p>
+    </div>
 
-        <div className="page-item">
-            <div className="item-title">
-                <h3>Beef salad / Can əti salatı</h3>
-                <p>14.80</p>
-            </div>
+ 
 
-            <div className="page-item-line"></div>
-            
-            <p>Beef tendertion, mix salad, lemon sauce, cherry tomato, walnut, red kidney beans</p>
-            <span><p>Can əti, göyərtilər, limon sousu, çerri pomidoru, qoz, qırmızı lobya</p></span>
-        </div>
+    <p>Beef tenderloin, mix salad, lemon sauce, cherry tomato, walnut, red kidney beans</p>
+    <span><p>Can əti, göyərtilər, limon sousu, çerri pomidoru, qoz, qırmızı lobya</p></span>
+</div>
 
-
-        <div className="page-item">
-            <div className="item-title">
-                <h3>Chicken popcorn salad / Toyuq popkorn salatı</h3>
-                <p>12.80</p>
-            </div>
-
-            <div className="page-item-line"></div>
-            
-            <p>Chichken popcorn, avocado, cherry tomato, corn, mix salad, popcorn sauce</p>
-            <span><p>Toyuq popkorn, avokado, çerri pomidoru qarğıdalı, göyərtilər, popkorn sousu</p></span>
-        </div>
+<div className="page-item">
+    <div className="item-title">
+        <h3>Chicken Popcorn Salad / Toyuq popkorn salatı</h3>
+        <p>13.80</p>
+    </div>
 
 
-        <div className="page-item">
-            <div className="item-title">
-                <h3>Caesar Salad / Sezar salatı</h3>
-                <p>12.80 / 14.80</p>
-            </div>
+    <p>Chicken popcorn, avocado, cherry tomato, corn, mix salad, popcorn sauce</p>
+    <span><p>Toyuq popkorn, avokado, çerri pomidoru, qarğıdalı, göyərtilər, popkorn sousu</p></span>
+</div>
 
-            <div className="page-item-line"></div>
-            
-            <p>Caesar sauce, iceberg, parmesan, cherry tomato, croutons with chichken fillet or prawn</p>
-            <span><p>Sezar sousu, aysberq, parmezan pendiri, çerri pomidoru, suxarı, toyuq filesi və ya krevet ilə</p></span>
-        </div>
+<div className="page-item">
+    <div className="item-title">
+        <h3>Caesar Salad / Sezar salatı</h3>
+        <p>13.80 / 16.80</p>
+    </div>
+
+  
+
+    <p>Caesar sauce, iceberg, parmesan, cherry tomato, croutons with chicken fillet or prawn</p>
+    <span><p>Sezar sousu, aysberq, parmezan pendiri, çerri pomidoru, suxarı, toyuq filesi və ya krevet ilə</p></span>
+</div>
+
+<div className="page-item">
+    <div className="item-title">
+        <h3>Tuna Salad / Tuna salatı</h3>
+        <p>15.40</p>
+    </div>
+
+   
+
+    <p>Tuna, boiled egg, mix salad, corn, olive, cherry tomato</p>
+    <span><p>Tuna balığı, qaynadılmış yumurta, göyərtilər, qarğıdalı, zeytun, çerri pomidoru</p></span>
+</div>
+
+<div className="page-item">
+    <div className="item-title">
+        <h3>Vegetarian Salad / Vegeterian salatı</h3>
+        <p>13.80</p>
+    </div>
+
+  
+
+    <p>Avocado, cucumber, cherry tomato, corn, olive, lemon sauce, white cheese, mix salad</p>
+    <span><p>Avokado, xiyar, çerri pomidoru, qarğıdalı, zeytun, limon sousu, ağ pendir, göyərtilər</p></span>
+</div>
 
 
-        <div className="page-item">
-            <div className="item-title">
-                <h3>Tuna salad / Tuna salatı</h3>
-                <p>12.00</p>
-            </div>
 
-            <div className="page-item-line"></div>
-            
-            <p>Tuna, boiled egg, mix salad, corn, olive, cherry tomato</p>
-            <span><p>Tuna balığı, qaynadılmış yumurta, göyərtilər, qarğıdalı, zeytun, çerri pomidoru</p></span>
-        </div>
+<div className='extra-title'>
+   <p> 5% service charge not included</p>
+   <p>5% xidmət haqqı qiymətlərə daxil deyildir</p>
+</div>
 
 
-        <div className="page-item">
-            <div className="item-title">
-                <h3>Vegetarian salad / Vegeterian salatı</h3>
-                <p>10.80</p>
-            </div>
-
-            <div className="page-item-line"></div>
-            
-            <p>Avocado, cucumber, cherry tomato, corn, olive, lemon sauce, white cheese, mix salad</p>
-            <span><p>Avokado, xiyar, çerri pomidoru, qarğıdalı, zeytun, limon sousu, ağ pendir, göyərtilər</p></span>
-        </div>
 
       </div>
 
